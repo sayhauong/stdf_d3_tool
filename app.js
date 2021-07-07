ffb28b54f868f71a75bcb437cc27fb23344b38c8
so101397 <SayHau.Ong@qorvo.com>
2021-07-07 19:02:32 +0200

    enable serer port

--- app.js
+++ app.js
@@ -321,10 +321,6 @@ app.get("/cdfplot", function(req, res){
 
 });
 
-// app.listen(process.env.PORT || 3000, function() {
-//   console.log("Server started on port 3000");
-// });
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
